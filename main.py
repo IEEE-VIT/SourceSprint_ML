3b9787e618d911567b076f5e7746334ff1613a19
Escanor-LionSin <100279039+Escanor-LionSin@users.noreply.github.com>
2023-09-21 02:34:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 from gensim.models import Word2Vec
 #Note: Extra modules may have to be imported
+#TO-DO: Clip configuration
+def image_to_prompt(image):
+    ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    ci.config.flavor_intermediate_count = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    image = image.convert('RGB')
+    a= ci.interrogate_fast(image)
+    return a
